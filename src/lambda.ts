d4eca5f9bf2c6e49731a9b940ed8641832e961df
abidkhan03 <abidkhan42935@gmail.com>
2023-08-16 15:18:21 +0500

    Adding carmaAPI

---
+++ src/lambda.ts
@@ -0,0 +1,29 @@
+import { configure as serverlessExpress } from '@vendia/serverless-express';
+import { NestFactory } from '@nestjs/core';
+import { Callback, Context, Handler } from 'aws-lambda';
+import { AppModule } from '@app/modules/main/app.module';
+import { ReplaySubject, firstValueFrom } from 'rxjs';
+
+const serverSubject = new ReplaySubject<Handler>();
+
+async function bootstrap(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 3000);
+
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
+}
+
+// Do not wait for lambdaHandler to be called before bootstraping Nest.
+// Pass the result of bootstrap() into the ReplaySubject
+bootstrap().then(server => serverSubject.next(server));
+
+
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+  // Convert the ReplaySubject into a Promise
+  // Wait for bootstrap to finish, then start handlign requests.
+  const server = await firstValueFrom(serverSubject)
+  return server(event, context, callback)
+};
+
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,37 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import 'source-map-support/register';
+import 'reflect-metadata';
+
+if (!process.env.IS_TS_NODE) {
+  require('module-alias/register');
+}
+
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
+
+import { AppModule } from '@app/modules/main/app.module';
+import { setupSwagger } from '@app/swagger';
+import { TrimStringsPipe } from '@app/modules/common/transformer/trim-strings.pipe';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
+  app.enableCors();
+  app.useGlobalPipes(
+    new TrimStringsPipe(),
+    new ValidationPipe({ whitelist: true }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
+
 bootstrap();
